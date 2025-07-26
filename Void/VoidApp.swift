cb82207a44967fffdbe340eb22fdf61a22b7ab57
GE-Developer <mikhail.bukhrashvili@gmail.com>
2025-07-26 15:57:28 +0400

    Initial Commit

---
+++ Void/VoidApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidApp.swift
+//  Void
+//
+//  Created by Mikhail Bukhrashvili on 26.07.25.
+//
+
+import SwiftUI
+
+@main
+struct VoidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
